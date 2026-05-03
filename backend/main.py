e58cf336f269a63ff52aaba06ad726ada55b761f
mehertpm <mehertpm@gmail.com>
2026-05-03 15:43:18 -0700

    Add files via upload

--- backend/main.py
+++ backend/main.py
@@ -198,5 +198,6 @@ async def ws_endpoint(websocket: WebSocket):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False,
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False,
                 log_level="info")
